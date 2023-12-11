70cb3eef887e5e709f886d35e6e720bd0616ea29
Yuseop <dbtjq1592000@gmail.com>
2023-12-11 15:10:02 +0900

    2일차 - Controller 학습

---
+++ TDD/tdd-test/src/main/java/com/test/tddtest/controller/PostController.java
@@ -0,0 +1,33 @@
+package com.test.tddtest.controller;
+
+import com.test.tddtest.dto.MemberDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@RequestMapping("/post-api")
+@RestController
+public class PostController {
+
+    @PostMapping("/member")
+    public String postMember(@RequestBody Map<String, Object> postData) {
+        StringBuilder sb = new StringBuilder();
+
+        postData.entrySet().forEach(
+                map -> {
+                    sb.append(map.getKey() + " : " + map.getValue() +"\n");
+                }
+        );
+
+        return sb.toString();
+    }
+
+    @PostMapping("/member2")
+    public String postMemberDto(@RequestBody MemberDTO memberDTO) {
+        return memberDTO.toString();
+    }
+
+}
---
+++ TDD/tdd-test/src/main/java/com/test/tddtest/dto/MemberDTO.java
@@ -0,0 +1,9 @@
+package com.test.tddtest.dto;
+
+import lombok.Data;
+@Data
+public class MemberDTO {
+    private String name;
+    private String email;
+    private String organization;
+}
