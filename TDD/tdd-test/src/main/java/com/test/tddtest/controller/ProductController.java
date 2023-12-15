8945252b7b0015155e22e1a8d67c96e3da49bd90
Yuseop <dbtjq1592000@gmail.com>
2023-12-15 16:45:03 +0900

    5일차 - Exception 적용

--- TDD/tdd-test/src/main/java/com/test/tddtest/controller/ProductController.java
+++ TDD/tdd-test/src/main/java/com/test/tddtest/controller/ProductController.java
@@ -6,9 +6,13 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/product")
@@ -53,5 +57,27 @@ public class ProductController {
     public ProductDto deleteProduct(@PathVariable String productId) {
         return null;
     }
+
+    @PostMapping("/exception")
+    public void exceptionTest() throws Exception {
+        throw new Exception();
+    }
+
+//    @ExceptionHandler(value = Exception.class)
+//    public ResponseEntity<Map<String, String>> ExceptinoHandler(Exception e) {
+//        HttpHeaders responseHeaders = new HttpHeaders();
+//        HttpStatus httpStatus = HttpStatus.BAD_REQUEST;
+//
+//        log.info(e.getLocalizedMessage());
+//        log.info("Controller 내 Exception Handler 호출");
+//
+//        Map<String, String> map = new HashMap<>();
+//        map.put("error type", httpStatus.getReasonPhrase());
+//        map.put("code", "400");
+//        map.put("message", "에러 발생");
+//
+//        return new ResponseEntity<>(map, responseHeaders, httpStatus);
+//
+//    }
 
 }
