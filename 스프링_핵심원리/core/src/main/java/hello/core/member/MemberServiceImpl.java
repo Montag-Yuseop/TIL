7667eac56abff092c9245f1d170e6f3b84b1fccb
Yuseop <dbtjq1592000@gmail.com>
2023-12-24 17:47:51 +0900

    :zap: feat: Test Code + MemberApp Create

--- 스프링_핵심원리/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ 스프링_핵심원리/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,8 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
+    // 인터페이스 뿐만아니라 구현체에 의존하고 있다.
+    // 추상 + 구현 모두 의존하고있다
 
     @Override
     public void join(Member member) {
