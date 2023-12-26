2992df74233a0f3c22d227092ac7d9c120278e3f
Yuseop <dbtjq1592000@gmail.com>
2023-12-26 21:14:54 +0900

    :zap: feat: AppConfig 생성

--- 스프링_핵심원리/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ 스프링_핵심원리/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,9 +2,14 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
     // 인터페이스 뿐만아니라 구현체에 의존하고 있다.
     // 추상 + 구현 모두 의존하고있다
+
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
