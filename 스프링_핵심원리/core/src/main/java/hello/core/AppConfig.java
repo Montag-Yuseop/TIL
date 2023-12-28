76625e1aca8b35b16484e4c79f7ef630b9126ce3
Yuseop <dbtjq1592000@gmail.com>
2023-12-28 16:30:57 +0900

    :zap: feat: Singleton 학습

--- 스프링_핵심원리/core/src/main/java/hello/core/AppConfig.java
+++ 스프링_핵심원리/core/src/main/java/hello/core/AppConfig.java
@@ -7,24 +7,36 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
+    // @Bean memberService -> new MemoryMemberRepository()
+    // @Bean orderService -> new MemoryMemberRepository()
+
+
     @Bean
     public MemberService memberService() {
+
+        System.out.println("call AppConfig.memberService");
+
         return new MemberServiceImpl(memberRepository());
     }
 
     @Bean
     public MemoryMemberRepository memberRepository() {
+
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
 
     @Bean
     public OrderService orderService() {
+
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
--- 스프링_핵심원리/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 스프링_핵심원리/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -24,4 +24,9 @@ public class OrderServiceImpl implements OrderService{
 
         return new Order(memberId, itemName, itemPrice, discountPrice); // 주문 생성
    }
+
+    // 테스트 용도
+    public MemberRepository getMemberRepository() {
+        return memberRepository;
+    }
 }
