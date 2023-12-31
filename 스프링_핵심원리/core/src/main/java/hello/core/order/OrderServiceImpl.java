401b22da4bbe9a0616e1485a7f1ccf6decb5ed04
Yuseop <dbtjq1592000@gmail.com>
2024-01-01 00:56:04 +0900

    :zap: feat: Lombok

--- 스프링_핵심원리/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 스프링_핵심원리/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,55 +4,18 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor // final 붙은 애들로 생성자를 만들어준다
 public class OrderServiceImpl implements OrderService{
 
-//    @Autowired
     private final MemberRepository memberRepository;
 
-    // 인터페이스에만 의존하도록 변경
-//    @Autowired
     private final DiscountPolicy discountPolicy;
-
-//    @Autowired
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        System.out.println("memberRepository = " + memberRepository);
-//        this.memberRepository = memberRepository;
-//    }
-//
-//    @Autowired
-//    public void setDiscountPolicy(DiscountPolicy discountPolicy) {
-//        System.out.println("discountPolicy = " + discountPolicy);
-//        this.discountPolicy = discountPolicy;
-//    }
-
-    // 생성자 주입
-    // 생성자 호출 시점에 딱 1번만 호출되는 것이 보장
-    // 불변, 필수 의존관계에 주로 사용된다
-//    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-        System.out.println("memberRepository = " + memberRepository);
-        System.out.println("discountPolicy = " + discountPolicy);
-        System.out.println("OrderServiceImpl.OrderServiceImpl");
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
-
-
-//    @Autowired
-//    public void setMemberRepository(MemberRepository memberRepository) {
-//        this.memberRepository = memberRepository;
-//    }
-
-//    @Autowired
-//    public void init(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
-
+    
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId); // 회원 정보 조회
