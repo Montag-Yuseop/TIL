dd6335d47ea53fd53e954e29994a1aa14b54a2aa
Yuseop <dbtjq1592000@gmail.com>
2024-01-02 02:05:43 +0900

    :zap: feat: primary, qualifier 적용

--- 스프링_핵심원리/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
+++ 스프링_핵심원리/core/src/main/java/hello/core/discount/RateDiscountPolicy.java
@@ -2,14 +2,16 @@ package hello.core.discount;
 
 import hello.core.member.Grade;
 import hello.core.member.Member;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("mainDiscountPolicy")
 public class RateDiscountPolicy implements DiscountPolicy {
 
+    private int discountPercent = 10;
     @Override
     public int discount(Member member, int price) {
-        int discountPercent = 10;
         if(member.getGrade() == Grade.VIP) return price * discountPercent / 100;
         return 0;
     }
--- 스프링_핵심원리/core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ 스프링_핵심원리/core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,19 +3,24 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor // final 붙은 애들로 생성자를 만들어준다
+//@RequiredArgsConstructor // final 붙은 애들로 생성자를 만들어준다
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
 
     private final DiscountPolicy discountPolicy;
-    
+
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+        this.memberRepository = memberRepository;
+        this.discountPolicy = discountPolicy;
+    }
+
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId); // 회원 정보 조회
