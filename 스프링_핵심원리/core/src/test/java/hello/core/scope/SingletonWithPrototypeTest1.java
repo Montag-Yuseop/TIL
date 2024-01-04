4e6c5ce97d179fc9c5494233c1d330d70d60a13b
Yuseop <dbtjq1592000@gmail.com>
2024-01-04 22:41:44 +0900

    :zap: feat: provider 오류 해결(jakarta)

--- 스프링_핵심원리/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ 스프링_핵심원리/core/src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,10 +2,12 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -33,27 +35,27 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
 
     }
 
     @Scope("singleton")
+    @Component
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
-
 
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
         public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             return prototypeBean.getCount();
         }
     }
 
+
     @Scope("prototype")
+    @Component
     static class PrototypeBean {
         private int count = 0;
 
