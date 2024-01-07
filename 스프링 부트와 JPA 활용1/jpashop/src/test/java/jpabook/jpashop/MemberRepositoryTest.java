8eedaa86e3697952eb0089e4d5ce0d67a2f91a01
Yuseop <dbtjq1592000@gmail.com>
2024-01-08 00:31:40 +0900

    :zap: feat: p6spy 설치 및 테스트 진행(Rollback false 적용)

--- 스프링 부트와 JPA 활용1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ 스프링 부트와 JPA 활용1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.*;
@@ -11,6 +12,7 @@ import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 @Transactional
+@Rollback(false) // 이걸 통해서 DB에 입력되게 할 수 있다
 class MemberRepositoryTest {
 
     @Autowired
@@ -30,6 +32,11 @@ class MemberRepositoryTest {
         // then
         assertThat(findMember.getId()).isEqualTo(member.getId());
         assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); // 저장한 객체와 조회한 객체는 같은 객체로 인식
+        
+        // 영속성 컨텍스트 내에서 같은 객체로 식별된다 -> 1차 캐시
+        System.out.println("member = " + member);
+        System.out.println("findMember = " + findMember);
 
     }
 
