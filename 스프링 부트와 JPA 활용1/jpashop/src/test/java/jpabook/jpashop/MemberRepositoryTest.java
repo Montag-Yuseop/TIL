895b546bb9810087740bd2ac9e64e715561f226a
Yuseop <dbtjq1592000@gmail.com>
2024-01-10 12:00:08 +0900

    :zap: feat: 엔티티 설계

--- 스프링 부트와 JPA 활용1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ 스프링 부트와 JPA 활용1/jpashop/src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -15,29 +15,29 @@ import static org.assertj.core.api.Assertions.*;
 @Rollback(false) // 이걸 통해서 DB에 입력되게 할 수 있다
 class MemberRepositoryTest {
 
-    @Autowired
-    MemberRepository memberRepository;
-
-    @Test
-    @DisplayName("멤버 테스트")
-    void testMember() throws Exception {
-        // given
-        Member member = new Member();
-        member.setUsername("memberA");
-
-        // when
-        Long saveId = memberRepository.save(member);
-        Member findMember = memberRepository.find(saveId);
-
-        // then
-        assertThat(findMember.getId()).isEqualTo(member.getId());
-        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
-        assertThat(findMember).isEqualTo(member); // 저장한 객체와 조회한 객체는 같은 객체로 인식
-        
-        // 영속성 컨텍스트 내에서 같은 객체로 식별된다 -> 1차 캐시
-        System.out.println("member = " + member);
-        System.out.println("findMember = " + findMember);
-
-    }
+//    @Autowired
+//    MemberRepository memberRepository;
+//
+//    @Test
+//    @DisplayName("멤버 테스트")
+//    void testMember() throws Exception {
+//        // given
+//        Member member = new Member();
+//        member.setUsername("memberA");
+//
+//        // when
+//        Long saveId = memberRepository.save(member);
+//        Member findMember = memberRepository.find(saveId);
+//
+//        // then
+//        assertThat(findMember.getId()).isEqualTo(member.getId());
+//        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+//        assertThat(findMember).isEqualTo(member); // 저장한 객체와 조회한 객체는 같은 객체로 인식
+//
+//        // 영속성 컨텍스트 내에서 같은 객체로 식별된다 -> 1차 캐시
+//        System.out.println("member = " + member);
+//        System.out.println("findMember = " + findMember);
+//
+//    }
 
 }
