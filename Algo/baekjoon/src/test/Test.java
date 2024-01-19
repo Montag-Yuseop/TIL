f7bd6cbe1a45a49df29c56ba1a51d642823879d4
Yuseop <dbtjq1592000@gmail.com>
2024-01-20 02:24:54 +0900

    진수 변환

--- Algo/baekjoon/src/test/Test.java
+++ Algo/baekjoon/src/test/Test.java
@@ -1,6 +1,17 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Test {
 
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println(Integer.parseInt(br.readLine(), 16));
+
+    }
 
 }
