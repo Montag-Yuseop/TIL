3ac60d7cae8b79088ca78454a3c29b536072b59d
Yuseop <dbtjq1592000@gmail.com>
2024-01-21 15:44:33 +0900

    백준 2752 세 수 정렬 - 기초, pq와 배열 활용

--- Algo/baekjoon/src/test/Test.java
+++ Algo/baekjoon/src/test/Test.java
@@ -3,14 +3,28 @@ package test;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Test {
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        System.out.println(Integer.parseInt(br.readLine(), 16));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for (int i = 0; i < 3; i++) {
+            int x = Integer.parseInt(st.nextToken());
+            pq.offer(x);
+        }
+
+        for (int i = 0; i < 3; i++) {
+            int x = pq.poll();
+            System.out.print(x +" ");
+        }
 
     }
 
