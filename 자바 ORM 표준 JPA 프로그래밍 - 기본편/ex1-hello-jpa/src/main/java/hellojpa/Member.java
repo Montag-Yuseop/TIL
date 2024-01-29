e120f98f57646259b0d04076e4506a3abad1b894
Yuseop <dbtjq1592000@gmail.com>
2024-01-30 02:33:26 +0900

    :zap: feat: 미사용 import 제거

--- 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ 자바 ORM 표준 JPA 프로그래밍 - 기본편/ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,10 +3,6 @@ package hellojpa;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.util.Date;
-
 @Entity
 @Getter
 @Setter
