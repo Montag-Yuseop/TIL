a9befd263040d84c616adda3c23f1ac288757501
Yuseop <dbtjq1592000@gmail.com>
2024-04-22 14:46:18 +0900

    :zap: feat: 상속 연습

--- Algo/baekjoon/src/test/Test.java
+++ Algo/baekjoon/src/test/Test.java
@@ -1,39 +1,34 @@
 package test;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+
+class A {
+    public void f() {
+        System.out.println("1");
+    }
+
+    public void g() {
+        System.out.println("2");
+    }
+
+}
+
+class B extends A {
+    public void f() {
+        System.out.println("3");
+    }
+}
+
+class C extends B {
+    public void g() {
+        System.out.println("4");
+    }
+}
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-
-        // a케이크b = a.z + b.x, a.y * b.y, a.x + b.z;
-        // a.z + b.x = c.x;
-        // a.y * b.y = c.y;
-        // a.x + b.z = c.z;
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int ax = Integer.parseInt(st.nextToken());
-        int ay = Integer.parseInt(st.nextToken());
-        int az = Integer.parseInt(st.nextToken());
-
-        st = new StringTokenizer(br.readLine());
-        int cx = Integer.parseInt(st.nextToken());
-        int cy = Integer.parseInt(st.nextToken());
-        int cz = Integer.parseInt(st.nextToken());
-
-        int bx = cx - az;
-        int by = cy / ay;
-        int bz = cz - ax;
-
-        System.out.println(bx +" " + by +" "+ bz);
-
-
+    public static void main(String[] args) {
+        A obj = new C();
+        obj.f();
+        obj.g();
 
     }
 
