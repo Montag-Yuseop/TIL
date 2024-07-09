bc52faa85fff14dfa348cac571ceeb2bf897c0b8
Yuseop <dbtjq1592000@gmail.com>
2024-07-10 07:36:47 +0900

    study: c++ 테스트

--- C++/코딩테스트 합격자 되기/namespace_테스트/서브스트링.cpp
+++ C++/코딩테스트 합격자 되기/namespace_테스트/서브스트링.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <vector>
 
 using std::cout;
 using std::cin;
 using std::string;
 using std::replace;
+using std::vector;
 
 int main() {
 	string s = "aBcDe";
 
-	s = s.substr(0, 2);
+	vector<int> arr;
+
+	s += "g";
+	s.append("1");
+	s.append("2");
 
 	cout << s;
+	
 
 	return 0;
 }
